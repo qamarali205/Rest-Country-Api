4255ffedb13c7c41a5fd07ab12a2d7a63be54cd1
qamarali205 <qamarali205@gmail.com>
2023-08-06 18:03:16 +0530

    country rest api

---
+++ country.js
@@ -0,0 +1,122 @@
+const countryName=new URLSearchParams(location.search).get('name');
+const flagImage=document.querySelector('.country-details img');
+const countryNameh=document.querySelector('.country-details h1');
+const nativeName=document.querySelector('.native-name');
+const population=document.querySelector('.population');
+const region=document.querySelector('.region');
+const subRegion=document.querySelector('.sub-region');
+const capital=document.querySelector('.capital');
+const topLevelDomain=document.querySelector('.top-level-domain');
+const currency=document.querySelector('.currency');
+const language=document.querySelector('.language');
+const borderCountrys=document.querySelector('.border');
+
+fetch(`https://restcountries.com/v3.1/name/${countryName}?fullText=true`)
+ .then((res)=> res.json())
+   .then(([country]) =>{
+
+       console.log(country);
+       flagImage.src=country.flags.svg;
+       countryNameh.innerText=country.name.common;
+       population.innerText=country.population.toLocaleString('en-IN');
+       region.innerText=country.region;
+       
+       if(country.subregion){
+        subRegion.innerText=country.subregion;
+       }
+       topLevelDomain.innerText=country.tld.join(', ');
+       
+       if(country.capital){
+        capital.innerText=country.capital;
+       }
+       if(country.subregion){
+        subRegion.innerText=country.subregion;
+       }
+       if(country.languages){
+        language.innerText=Object.values(country.languages).join(', ');
+       }
+       if(country.borders){
+        country.borders.forEach(border => {
+          // console.log(border)
+          fetch(`https://restcountries.com/v3.1/alpha/${border}`)
+            .then((res) => res.json())
+              .then(([borderCounry]) =>{
+                // console.log(borderCounry);
+                const borderCountryTag=document.createElement('a');
+                borderCountryTag.innerText=borderCounry.name.common;
+                borderCountryTag.href=`country.html?name=${borderCounry.name.common}`;
+                
+                borderCountrys.append(borderCountryTag);
+              })
+        });
+       }
+       if(country.name.nativeName){
+        nativeName.innerText=Object.values(country.name.nativeName)[0].common;
+       }else{
+        nativeName.innerText=country.name.common;
+       }
+       if(country.currencies){
+        currency.innerText=Object.values(country.currencies).map((currency) =>currency.name).join(', ');
+       }
+       if(country.border){
+
+       }
+       
+   })
+
+   //dark and light mode
+  //  const themeChange=document.querySelector('.themeChange');
+  //  themeChange.addEventListener('click', () => {
+  //   document.body.classList.toggle('dark')
+  //   themeChange.innerHTML=`<i class="fa-regular fa-moon"></i>&nbsp;&nbsp;Light Mode`;
+  // })
+
+
+  
+   //dark aur light mode with local storage 
+   const isDarkMode = localStorage.getItem('darkMode') === 'true';
+
+   // Update the text and icon based on the saved theme preference
+   if (isDarkMode) {
+     document.body.classList.add('dark');
+     themeChange.innerHTML = '<i class="fa-regular fa-moon"></i>&nbsp;&nbsp;Light Mode';
+   } else {
+     document.body.classList.remove('dark');
+     themeChange.innerHTML = '<i class="fa-solid fa-sun"></i>&nbsp;&nbsp;Dark Mode';
+   }
+   
+   // Toggle the theme and update text/icon on click
+   themeChange.addEventListener('click', () => {
+     const newTheme = !isDarkMode;
+     document.body.classList.toggle('dark', newTheme);
+   
+     if (newTheme) {
+       themeChange.innerHTML = '<i class="fa-regular fa-moon"></i>&nbsp;&nbsp;Light Mode';
+     } else {
+       themeChange.innerHTML = '<i class="fa-solid fa-sun"></i>&nbsp;&nbsp;Dark Mode';
+     }
+   
+     // Save the new theme preference in local storage
+     localStorage.setItem('darkMode', newTheme);
+   });
+
+
+   //scroll up 
+
+   var scrollTab = document.getElementById('scrollTab');
+
+   window.addEventListener('scroll', function() {
+     if (window.pageYOffset > 300) {
+       scrollTab.classList.add('show');
+     } else {
+       scrollTab.classList.remove('show');
+     }
+   });
+   
+   scrollTab.addEventListener('click', function(e) {
+     e.preventDefault();
+     window.scrollTo({
+       top: 0,
+       behavior: 'smooth'
+     });
+   });
---
+++ index.js
@@ -0,0 +1,121 @@
+const url='https://restcountries.com/v3.1/all';
+
+const countryContainer=document.querySelector('.countries-container');
+const filterByRegion=document.querySelector('#filter-by-region');
+const searchByCountry=document.querySelector('.search-container input');
+const themeChange=document.querySelector('.themeChange');
+
+
+let allCountryData;
+
+fetch(url)
+.then((res)=>res.json())
+.then((data) =>{
+    renderCountries(data);
+    allCountryData=data;
+
+});
+
+
+// filter data by region
+
+filterByRegion.addEventListener('change', (e) =>{
+    fetch(`https://restcountries.com/v3.1/region/${filterByRegion.value}`)
+        .then((res)=>res.json())
+        .then(renderCountries);
+
+})
+
+function renderCountries(data){
+    countryContainer.innerHTML='';
+    data.forEach(country => {
+         //console.log(country.name.common);
+        const countryCard=document.createElement('a');
+        countryCard.classList.add('country-card');
+        countryCard.href=`/country.html?name=${country.name.common}`;
+
+    
+        const cardHTML=`
+            <img src="${country.flags.svg}" alt="flag">
+            <div class="country-info">
+                <h3 class="country-name">${country.name.common}</h3>
+                <p><b>Population : </b>${country.population.toLocaleString('en-IN')}</p>
+                <p><b>Region : </b>${country.region}</p>
+                <p><b>Capital : </b>${country.capital}</p>
+            </div>
+            `;
+
+        countryCard.innerHTML=cardHTML;
+
+        countryContainer.append(countryCard);
+        
+    });
+    
+}
+
+searchByCountry.addEventListener('input', (e) =>{
+    allCountryData.filter((country) =>{
+        const filteredCountries = allCountryData.filter((country) => country.name.common.toLowerCase().includes(e.target.value.toLowerCase()))
+        renderCountries(filteredCountries);
+    })
+})
+
+//dark and light mode
+//  const themeChange=document.querySelector('.themeChange');
+//  themeChange.addEventListener('click', () => {
+//   document.body.classList.toggle('dark')
+//   themeChange.innerHTML=`<i class="fa-regular fa-moon"></i>&nbsp;&nbsp;Light Mode`;
+// })
+
+
+  
+//dark aur light mode with local storage 
+const isDarkMode = localStorage.getItem('darkMode') === 'true';
+
+// Update the text and icon based on the saved theme preference
+if (isDarkMode) {
+  document.body.classList.add('dark');
+  themeChange.innerHTML = '<i class="fa-regular fa-moon"></i>&nbsp;&nbsp;Light Mode';
+} else {
+  document.body.classList.remove('dark');
+  themeChange.innerHTML = '<i class="fa-solid fa-sun"></i>&nbsp;&nbsp;Dark Mode';
+}
+
+// Toggle the theme and update text/icon on click
+themeChange.addEventListener('click', () => {
+  const newTheme = !isDarkMode;
+  document.body.classList.toggle('dark', newTheme);
+
+  if (newTheme) {
+    themeChange.innerHTML = '<i class="fa-regular fa-moon"></i>&nbsp;&nbsp;Light Mode';
+  } else {
+    themeChange.innerHTML = '<i class="fa-solid fa-sun"></i>&nbsp;&nbsp;Dark Mode';
+  }
+
+  // Save the new theme preference in local storage
+  localStorage.setItem('darkMode', newTheme);
+});
+
+
+
+
+//scroll up 
+
+var scrollTab = document.getElementById('scrollTab');
+
+window.addEventListener('scroll', function() {
+  if (window.pageYOffset > 300) {
+    scrollTab.classList.add('show');
+  } else {
+    scrollTab.classList.remove('show');
+  }
+});
+
+scrollTab.addEventListener('click', function(e) {
+  e.preventDefault();
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+});
+
